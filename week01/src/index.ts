b4c59bedd72b5f37437a6fab68a28057fd9dcc00
bsh-ko <ssuh3872@gmail.com>
2025-03-26 02:31:49 +0900

    ✨ typescript 실습

---
+++ week01/src/index.ts
@@ -0,0 +1,31 @@
+const str1: string = "aa";
+// const str2: string = 123;
+
+const num1: number = 123;
+// const num2: number = "aa";
+
+const isMac: boolean = true;
+// const isMac2: boolean = "true";
+
+const isNull: null = null;
+// const isNull2: null = 123456789;
+
+const isUndefined: undefined = undefined;
+// const isUndefined2: undefined = null;
+
+const isSymbol: symbol = Symbol("symbol");
+// const isSymbol2: symbol = "Symbol";
+
+let bigNumber: bigint = 900930992547140991n;
+// let bigNumber2: bigint = 900930992547140991;
+
+const yaho: object = { yaho: "yaho" };
+
+let engName: { firstName: string; lastName: string } = {
+  firstName: "Ahn",
+  lastName: "Ohtani",
+};
+// let engName2: { firstName: number; lastName: string } = {
+//   firstName: "Ahn",
+//   lastName: "Ohtani"
+// };

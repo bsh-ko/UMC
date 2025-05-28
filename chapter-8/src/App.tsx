75774bf694d3099819ce59f89dd7657e9db7506d
bsh-ko <ssuh3872@gmail.com>
2025-05-28 21:00:07 +0900

    ✨ 장바구니 비우니 모달 기능

--- chapter-8/src/App.tsx
+++ chapter-8/src/App.tsx
@@ -4,6 +4,7 @@ import CartList from "./components/CartList";
 import Navbar from "./components/Navbar";
 import store from "./store/store";
 import PriceBox from "./components/PriceBox";
+import Modal from "./components/Modal";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <CartList />
       <PriceBox />
+      <Modal />
     </Provider>
   );
 }
--- chapter-8/src/components/PriceBox.tsx
+++ chapter-8/src/components/PriceBox.tsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "../hooks/useCustomRedux";
 import { clearCart } from "../slices/cartSlice";
+import { openModal } from "../slices/modalSlice";
 
 const Pricebox = () => {
   const { total } = useSelector((state) => state.cart);
@@ -9,10 +10,14 @@ const Pricebox = () => {
     dispatch(clearCart());
   };
 
+  const open = () => {
+    dispatch(openModal());
+  };
+
   return (
     <div className="flex justify-center p-8">
       <div className="flex justify-between p-20 w-full max-w-5xl items-center">
-        <button onClick={handleInitializeCart} className="border p-4 rounded-md cursor-pointer">
+        <button onClick={open} className="border p-4 rounded-md cursor-pointer">
           장바구니 초기화
         </button>
         <div className="bg-white shadow-md rounded-2xl border border-gray-200 px-8 py-6 text-lg font-semibold text-gray-800">
